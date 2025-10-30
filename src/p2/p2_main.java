293367b0cf5a5afc47008615b5934818cc3a5ae6
Karina <rybalchenkokatina310@gmail.com>
2025-10-30 17:45:55 +0100

    Aufgabe 2 Uebungblatt 2

---
+++ src/p2/p2_main.java
@@ -0,0 +1,15 @@
+package p2;
+
+public class p2_main {
+    public static void main(String[] args) {
+        int i = 5;        
+        boolean b = false; 
+
+        if (i > 10) {
+            b = true;
+        }
+
+        System.out.println("i = " + i);
+        System.out.println("b = " + b);
+    }
+}
